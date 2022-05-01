4e633c5760cf439d61f2b8c55c21e5ab1107922c
Artyom Zverev <artisst03@gmail.com>
2022-05-01 10:32:01 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gitopiad init --chain-id "$GITOPIA_CHAIN_ID" "$GITOPIA_MONIKER"
-curl -s "https://gitopia.com/gitopia1dlpc7ps63kj5v0kn5v8eq9sn2n8v8r5z9jmwff/testnets/tree/master/gitopia-janus-testnet/genesis.json" > $HOME/.gitopia/config/genesis.json
-
+gitopiad init --chain-id "$GITOPIA_CHAIN_ID" "$GITOPIA_MONIKER" &&\
+git clone gitopia://gitopia1dlpc7ps63kj5v0kn5v8eq9sn2n8v8r5z9jmwff/testnets &&\
+cp ./testnets/$GITOPIA_CHAIN_ID/genesis.json $HOME/.gitopia/config/genesis.json &&\
 gitopiad start
