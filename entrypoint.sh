5a320539bb9b0995940d22fa8e2da35f9545709b
Artyom Zverev <artisst03@gmail.com>
2022-05-01 10:17:53 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 gitopiad init --chain-id "$GITOPIA_CHAIN_ID" "$GITOPIA_MONIKER"
-curl -s "https://api.nodes.guru/gitopia_genesis_internal-7.json" > $HOME/.gitopia/config/genesis.json
+curl -s "https://raw.githubusercontent.com/stratosnet/stratos-chain-testnet/main/genesis.json" > $HOME/.gitopia/config/genesis.json
 
 gitopiad start
